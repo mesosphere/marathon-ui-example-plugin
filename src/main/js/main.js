6c67cd8382b3d067da79f92c0d0a04de46fea3a3
Orlando Hohmeier <orlando@mesosphere.io>
2016-02-05 19:09:40 +0100

    Move example plugin files

--- src/main/js/main.js
+++ src/main/js/main.js
@@ -1,1 +1,8 @@
-// main.js
+import ExamplePluginComponent from "components/ExamplePluginComponent";
+
+var {PluginHelper, PluginMountPoints} = global.MarathonUIPluginAPI;
+
+PluginHelper.registerMe("examplePlugin-0.0.1");
+
+PluginHelper.injectComponent(ExamplePluginComponent,
+  PluginMountPoints.SIDEBAR_BOTTOM);
